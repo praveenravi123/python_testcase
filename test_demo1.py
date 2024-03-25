70a5177d483db77cb6328290839fee322b6f2fbb
praveenravi123 <163387930+praveenravi123@users.noreply.github.com>
2024-03-25 15:17:17 +0530

    python

---
+++ test_demo1.py
@@ -0,0 +1,56 @@
+import pytest
+
+class Test_demo:
+
+    def test_one(self):
+        a=5
+        b=4
+        assert a>b, "a is not greater than b"
+
+    def test_two(self):
+        a=2
+        b=4
+        assert a<b, "a is not less than b"
+
+    def test_three(self):
+        a=4
+        b=4
+        assert a == b, "a is not equal than b"
+
+    def test_four(self):
+        a=3
+        b=4
+        assert a*b > 10, "a*b is not greater than 10"
+
+    def test_five(self):
+        a=5
+        b=4
+        assert a-b == 1, "a-b is not equla to 1"
+
+    def test_six(self):
+        a=1
+        b=4
+        assert a>b, "a is not greater than b"
+
+    def test_seven(self):
+        a = 6
+        b = 4
+        assert a < b, "a is not less than b"
+
+    def test_eigth(self):
+        a = 8
+        b = 4
+        assert a == b, "a is not equal than b"
+
+    def test_nine(self):
+        a = 2
+        b = 4
+        assert a * b > 10, "a*b is not greater than 10"
+
+    def test_ten(self):
+        a = 6
+        b = 4
+        assert a - b == 1, "a-b is not equla to 1"
+
+
+
